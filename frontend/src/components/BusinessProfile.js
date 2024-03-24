323f01da6b285edd0b8c3ac35a079a16f8863aba
realYashJoshi <114493214+realYashJoshi@users.noreply.github.com>
2024-03-24 12:14:49 +0530

    Update BusinessProfile.js

--- frontend/src/components/BusinessProfile.js
+++ frontend/src/components/BusinessProfile.js
@@ -1,89 +1,4 @@
 
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// const BusinessProfile = ({ user }) => {
-//   const [adContent, setAdContent] = useState('');
-//   const [users, setUsers] = useState([]);
-//   const [selectedUsers, setSelectedUsers] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch list of all users from MongoDB
-//     const fetchUsers = async () => {
-//       try {
-//         const res = await axios.get('http://localhost:5000/api/users');
-//         setUsers(res.data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-
-//     fetchUsers();
-//   }, []);
-
-//   const handleUserSelect = (userId) => {
-//     const updatedSelectedUsers = selectedUsers.includes(userId)
-//       ? selectedUsers.filter(id => id !== userId)
-//       : [...selectedUsers, userId];
-//     setSelectedUsers(updatedSelectedUsers);
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     // Submit ad with selected users
-//     try {
-//       await axios.post('http://localhost:5000/api/business/create-ad', {
-//         adContent,
-//         targets: selectedUsers,
-//         createdBy: user._id, 
-//       });
-//       // Reset form fields
-//       setAdContent('');
-//       setSelectedUsers([]);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//     console.log(selectedUsers);
-//   };
-
-//   return (
-//     <div className="container mt-4">
-//       <div className="card">
-//         <div className="card-header bg-warning">
-//           <h2 className="card-title">Business Profile</h2>
-//         </div>
-//         <div className="card-body">
-//           <p className="card-text">ID: {user._id}</p>
-//           <p className="card-text">Email: {user.email}</p>
-//           <form onSubmit={handleSubmit}>
-//             <div className="mb-3">
-//               <label htmlFor="adContent" className="form-label">Ad Content</label>
-//               <input type="text" className="form-control" id="adContent" value={adContent} onChange={(e) => setAdContent(e.target.value)} />
-//             </div>
-//             <div className="mb-3">
-//               <label className="form-label">Select Users</label>
-//               {users.map(user => (
-//                 <div key={user._id} className="form-check">
-//                   <input
-//                     type="checkbox"
-//                     className="form-check-input"
-//                     id={user._id}
-//                     checked={selectedUsers.includes(user._id)}
-//                     onChange={() => handleUserSelect(user._id)}
-//                   />
-//                   <label className="form-check-label" htmlFor={user._id}>{user.email}</label>
-//                 </div>
-//               ))}
-//             </div>
-//             <button type="submit" className="btn btn-primary">Create Ad</button>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default BusinessProfile;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -101,7 +16,7 @@ const BusinessProfile = ({ user }) => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const res = await axios.get('http://localhost:5000/api/users');
+        const res = await axios.get('https://terabh-intelligence.onrender.com/api/users');
         const viewerArray=res.data.filter((u) => (u.type!=='business'));
         
         setUsers(viewerArray);
@@ -153,7 +68,7 @@ const BusinessProfile = ({ user }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/business/create-ad', {
+      await axios.post(https://terabh-intelligence.onrender.com/api/business/create-ad', {
         adContent,
         imageUrl,
         targets: selectedUsers,
