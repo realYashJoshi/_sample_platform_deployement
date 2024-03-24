7410770f48d8db8311a5d894cab08dd7067b8bc1
realYashJoshi <114493214+realYashJoshi@users.noreply.github.com>
2024-03-24 12:13:14 +0530

    Update SignIn.js

--- frontend/src/authPages/SignIn.js
+++ frontend/src/authPages/SignIn.js
@@ -1,60 +1,4 @@
-// SignIn.js
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import Cookies from 'js-cookie';
-// import { useNavigate } from 'react-router-dom';
-// import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
-// const SignIn = () => {
-//   const [userData, setUserData] = useState({ email: '', password: '' });
-//   const navigate = useNavigate();
-
-//   const handleChange = (e) => {
-//     setUserData({ ...userData, [e.target.name]: e.target.value });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const { data } = await axios.post('http://localhost:5000/api/auth/signin', userData);
-//       if(!data)navigate('/signin')
-//       Cookies.set('token', data.token, { expires: 10, secure: true }); // Set cookie with token
-      
-//       navigate('/profile');
-//     console.log("user signed in successfully",data);
-//     } catch (error) {
-//       console.error(error);
-//       alert("Wrong Credentials");
-//     }
-//   };
-
-//   return (
-//     <Container className="mt-5">
-//       <Row className="justify-content-center">
-//         <Col xs={12} md={6}>
-//           <h2 className="mb-4 text-center">Sign In</h2>
-//           <Form onSubmit={handleSubmit}>
-//             <Form.Group controlId="formBasicEmail">
-//               <Form.Label>Email address</Form.Label>
-//               <Form.Control type="email" placeholder="Enter email" name="email" value={userData.email} onChange={handleChange} required />
-//             </Form.Group>
-
-//             <Form.Group controlId="formBasicPassword">
-//               <Form.Label>Password</Form.Label>
-//               <Form.Control type="password" placeholder="Password" name="password" value={userData.password} onChange={handleChange} required />
-//             </Form.Group>
-//              <br></br>
-//             <Button variant="primary" type="submit" className="w-100">
-//               Sign In
-//             </Button>
-//           </Form>
-//         </Col>
-//       </Row>
-//     </Container>
-//   );
-// };
-
-// export default SignIn;
 import React, { useState } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
@@ -74,7 +18,7 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post('http://localhost:5000/api/auth/signin', userData);
+      const { data } = await axios.post('https://terabh-intelligence.onrender.com/api/auth/signin', userData);
       if(!data) navigate('/signin')
       Cookies.set('token', data.token, { expires: 10, secure: true }); // Set cookie with token
       navigate('/profile');
